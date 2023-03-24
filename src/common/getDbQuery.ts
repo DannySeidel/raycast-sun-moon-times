f7736e9f1b54f046ee8612109a20a4539f373f55
Danny Seidel <69514484+DannySeidel@users.noreply.github.com>
2023-03-24 17:29:46 +0100

    optimized db query

--- src/common/getDbQuery.ts
+++ src/common/getDbQuery.ts
@@ -1,12 +1,23 @@
 export function getDbQuery(searchText: string): string {
-    if (searchText.length < 3) {
+    // display saved cities if no search text
+    if (searchText.length === 0) {
         return `SELECT id
                 FROM cities
-                WHERE name LIKE '----';`
-    } else {
+                WHERE name like '----';`
+    }
+
+    // display cities with population > 100,000 if search text is 2 characters or less to avoid heap overflow
+    if (searchText.length < 3) {
         return `SELECT id, name, country, lon, lat
                 FROM cities
-                WHERE name LIKE '%${searchText}%'
+                WHERE name LIKE '${searchText}%'
+                  AND population > 100000
                 ORDER BY population DESC;`
     }
+
+    // display cities if search text is 3 characters or more
+    return `SELECT id, name, country, lon, lat
+            FROM cities
+            WHERE name LIKE '%${searchText}%'
+            ORDER BY population DESC;`
 }
--- src/common/resolveCoords.ts
+++ src/common/resolveCoords.ts
@@ -2,5 +2,5 @@ export function resolveCoords(lat: number, lon: number): string {
     const latOrientation = lat < 0 ? "S" : "N"
     const lonOrientation = lon < 0 ? "W" : "E"
 
-    return `${latOrientation} ${lat.toFixed(2)}, ${lonOrientation} ${lon.toFixed(2)}`
+    return `${latOrientation} ${Math.abs(lat).toFixed(2)}, ${lonOrientation} ${Math.abs(lon).toFixed(2)}`
 }
